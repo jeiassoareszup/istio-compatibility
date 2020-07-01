629a414a7a03138c7cd8424f47176764f6de5cc1
jeiassoareszup <jeias.soares@zup.com.br>
2020-07-01 11:18:21 -0300

    adding log

--- src/main/java/com/example/istio/demo/IstioController.java
+++ src/main/java/com/example/istio/demo/IstioController.java
@@ -1,16 +1,22 @@
 package com.example.istio.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestController
 public class IstioController {
 
+    Logger logger = LoggerFactory.getLogger(IstioController.class);
+
     @GetMapping("/istio")
     public Map<String, String> istio(@RequestHeader Map<String, String> headers){
+        logger.info("Request received at {}: GET /istio ", LocalDateTime.now());
         return headers;
     }
 }
